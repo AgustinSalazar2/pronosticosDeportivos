39db2294dbb6f1e8d239aec7e067579106f52a9c
lauraveronic01 <lau.veronica01@gmail.com>
2023-03-19 17:53:13 -0300

    Creacion de rama Lau, modificacion del enum

--- src/clases/ResultadoEnum.java
+++ src/clases/ResultadoEnum.java
@@ -1,34 +1,9 @@
 package clases;
 
-public class ResultadoEnum {
-    public String ganador = "GANADOR";
-    public String perdedor = "PERDEDOR";
-    public String empate = "EMPATE";
+public enum ResultadoEnum {
+    GANADOR,
+    PERDEDOR,
+    EMPATE
 
-    public ResultadoEnum() {
-    }
 
-    public String getGanador() {
-        return ganador;
-    }
-
-    public void setGanador(String ganador) {
-        this.ganador = ganador;
-    }
-
-    public String getPerdedor() {
-        return perdedor;
-    }
-
-    public void setPerdedor(String perdedor) {
-        this.perdedor = perdedor;
-    }
-
-    public String getEmpate() {
-        return empate;
-    }
-
-    public void setEmpate(String empate) {
-        this.empate = empate;
-    }
 }
